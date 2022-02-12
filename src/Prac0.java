0c0195480efe5830505db19a827399c0306b33aa
alexrglez <alexrglez@LAPTOP-OI7M48EM>
2022-02-12 12:01:06 +0100

    Modif. master

--- src/Prac0.java
+++ src/Prac0.java
@@ -4,7 +4,8 @@ public class Prac0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String uo="UO276609";
-		int num=25;
+		int num=25;		
+		System.out.println("Modificación master");
 
 	}
 
